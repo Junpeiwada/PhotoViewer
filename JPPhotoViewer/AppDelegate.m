bad8430cf0d0358b1a8aaf888debda4d470e6de5
junpeiwada <junpeiwada@gmail.com>
2016-08-18 10:53:12 +0900

    TintColorを黒にした

--- JPPhotoViewer/AppDelegate.m
+++ JPPhotoViewer/AppDelegate.m
@@ -18,6 +18,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window.tintColor = [UIColor blackColor];
     return YES;
 }
 
